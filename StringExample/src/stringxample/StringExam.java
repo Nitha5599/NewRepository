d7b6993e398ebca41fc0bea9752e1f3201587b1a
smithamurali <smithamurali970@gmail.com>
2024-09-12 21:50:10 +0530

    String Example

---
+++ StringExample/src/stringxample/StringExam.java
@@ -0,0 +1,20 @@
+package stringxample;
+
+public class StringExam {
+
+	public static void main(String[] args) {
+		String str1 = "Hai";
+		String str2 = "Java";
+		System.out.println(str1.concat(str2));
+		System.out.println(str1.length());
+		String s1 = new String("Nitha");
+		String s2 = new String("Murali");
+		String s3 = new String("Nitha");
+		System.out.println(s1.concat(s2));
+		System.out.println(s1.length());
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.equals(s3));
+
+	}
+
+}
